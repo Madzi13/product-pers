1880ea590f897903e710a2f6c2563dfa3f1b6ac0
Magdalena Stryszowska <madzi13@op.pl>
2023-10-15 21:16:49 +0200

    product_2

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,76 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from "react";
+import shortid from 'shortid';
 
 const Product = props => {
+  const {name, title, basePrice, colors, sizes} = props;
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+  const getPrice = () => {
+    const additionalPrice = sizes.find(size => size.name === currentSize).additionalPrice;
+    return basePrice + additionalPrice;
+  };
+
+  const addToCart = event => {
+    event.preventDefault();
+    const summary = {
+      Name: title,
+      Size: currentSize,
+      Price: getPrice(),
+      Color: currentColor,
+    }
+
+    console.log('Product Summary:', summary);
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={'${title}'}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>{getPrice()}$</span>
         </header>
-        <form>
+        <form onSubmit={addToCart}>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+            {sizes.map((size) => (
+                <li key={shortid()}>
+                  <button
+                    type="button"
+                    className={clsx(size.name === currentSize && styles.active)}
+                    onClick={() => setCurrentSize(size.name)}
+                  >
+                    {size.name}
+                  </button>
+                </li>
+              ))} 
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+            {colors.map((color) => (
+                <li key={color}>
+                  <button
+                    type="button"
+                    className={clsx(prepareColorClassName(color), color === currentColor && styles.active)}
+                    onClick={() => setCurrentColor(color)}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -41,6 +80,13 @@ const Product = props => {
       </div>
     </article>
   )
+};
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.array.isRequired,
+  sizes: PropTypes.array.isRequired,
 };
 
 export default Product;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
